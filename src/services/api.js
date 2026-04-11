2fa9e6d903cbce72a4a8e6afb2ce13fbe188fbff
M Rafif Musyaffa <88952324+M-Rafif-Musyaffa@users.noreply.github.com>
2026-04-11 13:48:18 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// Membuat "telepon khusus" yang nomor awalnya (baseURL) sudah disave
 const api = axios.create({
   // Ini adalah alamat utama server pembuat data Al-Quran
   baseURL: 'https://equran.id/api/v2',
