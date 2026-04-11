9b76e5f6e96dd38fef0bd3902f48ae423556b318
M Rafif Musyaffa <88952324+M-Rafif-Musyaffa@users.noreply.github.com>
2026-04-11 13:48:37 +0700

    Update doaApi.js

--- src/services/doaApi.js
+++ src/services/doaApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// Kita ganti pakai server EQuran.id yang jauh lebih besar dan stabil
+// Server EQuran.id 
 const doaApi = axios.create({
   baseURL: 'https://equran.id/api',
   timeout: 10000,
